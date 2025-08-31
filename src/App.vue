39a768db580d82b5788a4b8997cfc70d2a1584ea
JessieYuuuuu <readyu9998@gmail.com>
2025-08-31 21:22:00 +0800

    refactor：導覽列組件重構

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <nav-bar />
-  <div class="pt-[80px]">
-    <div class="h-[calc(100vh-80px)] flex items-center justify-center">
-      <RouterView />
-    </div>
+  <nav-bar :list="navLinks" />
+  <div class="h-[calc(100vh-80px)] flex items-center justify-center overflow-y-auto py-10">
+    <RouterView />
   </div>
 </template>
 <script setup>
 import NavBar from './components/NavBar.vue';
+const navLinks = [
+  { name: '首頁', path: '/' },
+  { name: '第一周作業', path: '/week-one' },
+  { name: '第二周作業', path: '/week-two' },
+  { name: '第三周作業', path: '/week-three' },
+  { name: '進階任務', path: '/advanced-missions' },
+];
 </script>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,18 @@
 <template>
-  <nav class="fixed top-0 left-0 z-50 w-full h-[80px] bg-amber-500 flex items-center justify-between px-6">
-    <RouterLink to="/">首頁</RouterLink>
-    <RouterLink to="/week-one">第一周作業</RouterLink>
-    <RouterLink to="/week-two">第二周作業</RouterLink>
+  <nav class=" top-0 z-50 w-full h-[80px] bg-amber-500 flex items-center px-6">
+    <ul class="flex gap-6 justify-between w-full">
+      <li v-for="item in list" :key="item.path">
+        <RouterLink :to="item.path">{{ item.name }}</RouterLink>
+      </li>
+    </ul>
   </nav>
 </template>
+<script setup>
+const props = defineProps({
+  list: {
+    type: Array,
+    default: [],
+  }
+});
+console.log(props.list);
+</script>
