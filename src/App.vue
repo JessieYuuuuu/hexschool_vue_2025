513c73c1c3d669e0fc61b620693813b0af7cd2d2
JessieYuuuuu <readyu9998@gmail.com>
2025-09-07 01:23:20 +0800

    feat：根據路由修改主佈局

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <nav-bar :list="navLinks" />
-  <div class="h-[calc(100vh-80px)] flex items-center justify-center overflow-y-auto py-10">
+  <nav-bar v-if="!isInFinalMission" :list="navLinks" />
+  <div :class="!isInFinalMission ? 'h-[calc(100vh-80px)] flex items-center justify-center overflow-y-auto py-10' : ''">
     <RouterView />
   </div>
 </template>
 <script setup>
 import NavBar from './components/NavBar.vue';
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+// 根據路由決定是否顯示navBar
+const isInFinalMission = computed(() => { return route.matched[0]?.name === '最終任務' });
+
 const navLinks = [
   { name: '首頁', path: '/' },
   { name: '第一周作業', path: '/week-one' },
   { name: '第二周作業', path: '/week-two' },
   { name: '第三周作業', path: '/week-three' },
   { name: '進階作業', path: '/advanced-missions' },
-  { name: '最終任務', path: '/final-missions' },
+  { name: '最終任務', path: '/final-todo-list' },
 ];
 </script>
